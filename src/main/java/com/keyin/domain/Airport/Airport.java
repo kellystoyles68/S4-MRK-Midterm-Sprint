876c2134ff0a00d3b27158759ca2a62d4fd8f3da
kellystoyles <147680273+kellystoyles@users.noreply.github.com>
2024-10-24 13:24:41 -0230

    Update Airport.java

--- src/main/java/com/keyin/domain/Airport/Airport.java
+++ src/main/java/com/keyin/domain/Airport/Airport.java
@@ -1,7 +1,9 @@
-package com.keyin.domain.Airport;
+package com.keyin.domain;
 
-import com.keyin.domain.Aircraft.Aircraft;
 import jakarta.persistence.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -53,5 +55,22 @@ public class Airport {
 
     public void setIATA_code(String IATA_code) {
         this.IATA_code = IATA_code;
+    }
+
+    @Repository
+    public static interface AirportRepository extends JpaRepository<Airport, Long> {
+        @Query("SELECT a.city.province, COUNT(a) FROM Airport a GROUP BY a.city.province HAVING COUNT(a) > 1")
+        List<Object[]> findProvincesWithMultipleAirports();
+
+    }
+
+    @Repository
+    public static interface CityRepository extends JpaRepository<City, Long> {
+        @Query("SELECT c.province, SUM(c.population), COUNT(a) FROM City c LEFT JOIN c.airports a GROUP BY c.province HAVING SUM(c.population) BETWEEN 900000 AND 1100000")
+        List<Object[]> findProvincesWithPopulationAndAirports();
+
+        @Query("SELECT c.name, c.province, c.population, SUM(c2.population) as provincePopulation FROM City c JOIN City c2 ON c.province = c2.province WHERE EXISTS (SELECT 1 FROM Airport a WHERE a.city = c) GROUP BY c.name, c.province, c.population")
+        List<Object[]> findCityProvincePopulationComparison();
+
     }
 }
