149fe30eca9cfa55280221f91c6ce646c8f4f243
kellystoyles <147680273+kellystoyles@users.noreply.github.com>
2024-10-24 13:23:47 -0230

    Update AircraftRepository.java

--- src/main/java/com/keyin/domain/Aircraft/AircraftRepository.java
+++ src/main/java/com/keyin/domain/Aircraft/AircraftRepository.java
@@ -1,9 +1,11 @@
-package com.keyin.domain.Aircraft;
+package com.keyin.domain;
 
+import com.keyin.domain.Aircraft;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AircraftRepository extends JpaRepository<Aircraft, Long> {
+
 
 }
